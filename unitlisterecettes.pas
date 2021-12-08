7f25a48e9afeddb59d6526c4314103b77c6cfd60
Louis DEVIE <louisdevie@gmail.com>
2021-12-08 11:36:07 +0100

    modèle d'implémentation des fonctions exportées

--- unitlisterecettes.pas
+++ unitlisterecettes.pas
@@ -17,6 +17,9 @@ interface
     procedure insererRecette(recette: TypeRecette);
 
 
+    procedure effacerRecettes;
+
+
     procedure premierePageRecettes;
 
     procedure pageSuivanteRecettes;
@@ -30,6 +33,66 @@ interface
 
 
 implementation
+
+    type
+        PtrCelluleRecette = ^CelluleRecette;
+
+        CelluleRecette = record
+            valeur: TypeRecette;
+            precedantOrdreAlpha: PtrCelluleRecette;
+            suivantOrdreAlpha: PtrCelluleRecette;
+            precedantOrdreBonus: PtrCelluleRecette;
+            suivantOrdreBonus: PtrCelluleRecette;
+        end;
+
+    var
+        _pointeurPage: PtrCelluleRecette;
+        _pointeurRecette: PtrCelluleRecette;
+        _pageCourante: Integer;
+
+    procedure initRecettes;
+    begin
+        // À FAIRE
+    end;
+
+
+    procedure insererRecette(recette: TypeRecette);
+    begin
+        // À FAIRE
+    end;
+
+
+    procedure effacerRecettes;
+    begin
+        // À FAIRE
+    end;
+
+
+    procedure premierePageRecettes;
+    begin
+        // À FAIRE
+    end;
+
+    procedure pageSuivanteRecettes;
+    begin
+        // À FAIRE
+    end;
+
+    procedure pagePrecedenteRecettes;
+    begin
+        // À FAIRE
+    end;
+
+    function pageCourante: Integer;
+    begin
+        // À FAIRE
+    end;
+
+
+    function lireRecette(n: Integer): TypeRecette;
+    begin
+        // À FAIRE
+    end;
 
 end.
 
