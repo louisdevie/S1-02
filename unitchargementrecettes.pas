7e50bc4e3a623e5fd091c3ff961db2e0f8aaf662
Louis DEVIE <louisdevie.contact@gmail.com>
2021-12-15 16:58:32 +0100

    fusion avec le jeu existant

---
+++ unitchargementrecettes.pas
@@ -0,0 +1,70 @@
+unit unitChargementRecettes;
+
+interface
+
+    procedure chargerLesRecettes;
+
+implementation
+
+    uses unitListeRecettes;
+
+    function bonusFromString(bonus: string): EnumBonus;
+    begin
+        if bonus = '(Regeneration)' then
+            bonusFromString := REGENERATION
+        else if bonus = '(Critique)' then
+            bonusFromString := CRITIQUE
+        else if bonus = '(Force)' then
+            bonusFromString := FORCE
+        else
+            bonusFromString := AUCUN;
+    end;
+
+    // sépare une chaine de caractères au niveau du dernier "/" trouvé
+    procedure separer(ligne: string; var avant, apres: string);
+    var
+        i: Integer; // indice pour parcourir la ligne
+    begin
+        i := length(ligne); // on part de la fin
+        avant := '';
+        apres := '';
+        // `apres` contient tout ce qu'il y a après le dernier "/"
+        while (ligne[i] <> '/') and (i > 0) do begin
+            apres := ligne[i] + apres;
+            i -= 1;
+        end;
+        // on saute le "/"
+        i -= 1;
+        // `avant` contient ce qu'il reste
+        while (i > 0) do begin
+            avant := ligne[i] + avant;
+            i -= 1;
+        end;
+    end;
+
+    procedure chargerLesRecettes;
+    var
+        fichier: Text;
+        ligne, nom, effet: string;
+        recette: TypeRecette;
+    begin
+        AssignFile(fichier, 'Recettes.txt');
+
+        reset(fichier);
+
+        initRecettes;
+
+        while not eof(fichier) do
+        begin
+            readln(fichier, ligne);
+            separer(ligne, nom, effet);
+            recette.nom := nom;
+            recette.effet := bonusFromString(effet);
+            insererRecette(recette);
+        end;
+
+        closeFile(fichier);
+        recettesChargees;
+    end;
+end.
+
